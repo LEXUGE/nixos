2e90c479fc11e78661888c0a3cc6e37aa9152912
Harry Ying <lexugeyky@outlook.com>
2020-03-02 18:55:36 +0800

    Add status outputs to sync.sh

--- sync.sh
+++ sync.sh
@@ -2,6 +2,7 @@
 
 rm -rf ./*.nix ./dotfiles/ ./packages/ ./modules/ ./overlays/
 
+echo -n "Copying..."
 cp -a /etc/nixos/secrets/*.example ./secrets/.
 cp -a /etc/nixos/*.nix ./
 cp -a /etc/nixos/dotfiles ./
@@ -9,9 +10,18 @@ cp -a /etc/nixos/packages ./
 cp -a /etc/nixos/modules ./
 cp -a /etc/nixos/overlays ./
 find . -type f -name '*.nix' -exec nixfmt {} +
+echo -n "Done."
 
+echo -n "Adding to git..."
 git add --all
+echo -n "Done."
+
+echo -n "Commiting..."
 echo "Enter commit message: "
 read commitMessage
 git commit -m "$commitMessage"
+echo -n "Done."
+
+echo -n "Pushing..."
 git push
+echo -n "Done."
