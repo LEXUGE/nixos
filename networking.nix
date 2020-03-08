9ea5368a6849a867eba428eb4582440a68837934
Harry Ying <lexugeyky@outlook.com>
2020-03-09 00:20:36 +0800

    Add option to set hostname

--- networking.nix
+++ networking.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = config.lib.system.hostname; # Define hostname.
 
   # Allow Spotify Local discovery
   networking.firewall.allowedTCPPorts = [ 57621 ];
