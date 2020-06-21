593fa18a3831393aefe00f51718ef56adc6704be
Harry Ying <lexugeyky@outlook.com>
2020-06-21 11:54:10 +0800

    Fix hashedPassword warning for root

--- plugins/system/clash/packages/yacd.nix
+++ plugins/system/clash/packages/yacd.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "yacd";
-  version = "v0.1.21";
+  version = "v0.1.22";
 
   src = fetchzip {
     url = "https://github.com/haishanh/yacd/archive/gh-pages.zip";
-    sha256 = "1z15xywyax0q2fidvk4n1ixsfb9zsd7ybzxwzsj494ddxikj1216";
+    sha256 = "05wrgzlc6m2n6y6aa276qk7c8d48rc4k6bvyayrgm3gj99zf5ybw";
   };
 
   phases = [ "installPhase" ];
