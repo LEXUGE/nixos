5d0f7e3b30e60feddd2c36e04bf4943c4e16d0ba
Harry Ying <lexugeyky@outlook.com>
2020-08-01 10:05:20 +0800

    Optional iwd; disable iwd

--- plugins/system/x-os/networking.nix
+++ plugins/system/x-os/networking.nix
@@ -15,29 +15,32 @@ in {
       description = "Binary caches to use.";
     };
     iwdConfig = mkOption {
-      type = with types; attrsOf (attrsOf (oneOf [ bool int str ]));
-      default = { };
+      type = with types; nullOr (attrsOf (attrsOf (oneOf [ bool int str ])));
+      default = null;
       description = "Configuratoin of iNet Wireless Daemon.";
     };
   };
-  config = mkIf cfg.enable {
-    networking.hostName = cfg.hostname; # Define hostname
+  config = mkIf cfg.enable (mkMerge [
+    ({
+      networking.hostName = cfg.hostname; # Define hostname
 
-    networking.networkmanager = {
-      # Enable networkmanager. REMEMBER to add yourself to group in order to use nm related stuff.
-      enable = true;
-      # Don't use DNS advertised by connected network. Use local configuration
-      dns = "none";
-      # Use iwd instead
-      wifi.backend = "iwd";
-    };
+      networking.networkmanager = {
+        # Enable networkmanager. REMEMBER to add yourself to group in order to use nm related stuff.
+        enable = true;
+        # Don't use DNS advertised by connected network. Use local configuration
+        dns = "none";
+      };
 
-    environment.etc."iwd/main.conf".text = generators.toINI { } cfg.iwdConfig;
+      # Customized binary caches list (with fallback to official binary cache)
+      nix.binaryCaches = cfg.binaryCaches;
 
-    # Customized binary caches list (with fallback to official binary cache)
-    nix.binaryCaches = cfg.binaryCaches;
+      # Use local DNS server all the time
+      networking.resolvconf.useLocalResolver = true;
+    })
 
-    # Use local DNS server all the time
-    networking.resolvconf.useLocalResolver = true;
-  };
+    (mkIf (cfg.iwdConfig != null) {
+      environment.etc."iwd/main.conf".text = generators.toINI { } cfg.iwdConfig;
+      networking.networkmanager.wifi.backend = "iwd";
+    })
+  ]);
 }
