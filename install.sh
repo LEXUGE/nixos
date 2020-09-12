641e7e04cfd48ec1ed769a804f58cd9576c4f428
Harry Ying <lexugeyky@outlook.com>
2020-09-12 22:02:48 +0800

    Add hardware-configuration back (dummy config actually)

--- install.sh
+++ install.sh
@@ -72,6 +72,7 @@ nixos_install() {
 	git clone https://github.com/LEXUGE/nixos ${MOUNTPOINT}/etc/nixos/
 
 	rm ${MOUNTPOINT}/etc/nixos/secrets/keyfile.bin
+	rm ${MOUNTPOINT}/etc/nixos/hardware-configuration.nix
 
 	create_keyfile
 	reset
