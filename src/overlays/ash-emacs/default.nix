b1fdae19df3837e373ff9ac4dd871bd319c3821a
Harry Ying <lexugeyky@outlook.com>
2020-09-17 09:05:54 +0800

    Use emacsGcc

--- src/overlays/ash-emacs/default.nix
+++ src/overlays/ash-emacs/default.nix
@@ -17,5 +17,6 @@ in (final: prev:
     attrsets.nameValuePair ("ash-emacs-${system}")
     ((importer system).emacsWithPackagesFromUsePackage {
       config = concatEmacsConfig ../../../dotfiles/ash/emacs.d/elisp;
+      package = (importer system).pkgs.emacsGcc;
       alwaysEnsure = true;
     })) flake-utils.lib.defaultSystems))
