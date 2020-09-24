4746910d47d0ee1e4035cf1fbf4183d92f4340c1
Harry Ying <lexugeyky@outlook.com>
2020-09-24 21:09:43 +0800

    Remove rcon for minecraft server

--- src/minecraft-server.nix
+++ src/minecraft-server.nix
@@ -36,18 +36,20 @@
       Mac-GM = "e261565d-0856-3d15-b3ae-401014fc10fd"; # Billy Xu - Offline
       # AshBreaker1 = "65bec9be-2cb8-46c8-bab5-2a5219759a4a"; # Online
       mick233 = "d32170e2-5cd8-35b5-9fac-2c71854318ef";
+      sam_shen = "c83f7303-19fa-350c-a94f-d5dca5a03c52"; # Sam Shen - Offline
     };
 
     serverProperties = {
       online-mode = false;
       max-players = 30;
-      level-name = "newera";
+      level-name = "newera2";
       white-list = true;
-      enable-rcon = true;
-      difficulty = "hard";
-      "rcon.password" = "nixos";
+      #enable-rcon = true;
+      difficulty = "normal";
+      #"rcon.password" = "nixos";
       network-compression-threshold =
         64; # Compress any packets larger than 64 bytes
+      max-world-size = 2000;
       motd =
         "\\u00A76NewEra \\u00A77Vanilla \\u00A7cSurvival\\u00A7r\\n\\u00A7bt.me/NewEraMinecraft";
     };
