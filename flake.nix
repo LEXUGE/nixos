96522fcb30709362f169890b09886b42606e2837
Harry Ying <lexugeyky@outlook.com>
2020-10-01 11:59:24 +0800

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Harry Ying's NixOS configuration";
 
   inputs = {
-    nixos.url = "github:NixOS/nixpkgs-channels/nixos-unstable";
+    nixos.url = "github:NixOS/nixpkgs/nixos-unstable";
     home = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixos";
